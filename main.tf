f45818f3ac4c81e54f5ab1daf8ee448aea56e9e3
chrisarnott86 <chrisarnott86@hotmail.com>
2026-05-08 13:44:46 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "e8539dfe-9caa-4508-a0cf-d7961e3f13ec"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG1"
+    storage_account_name = "<your storage account>"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "RG2"
+  location = "westeurope"
+}
